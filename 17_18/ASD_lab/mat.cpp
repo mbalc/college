35050eea9b52529e86f8ac9c0adc562f6c7e5903
mbalc <mbalc909@gmail.com>
2017-10-06 14:25:32 +0200

    Add empty mat.cpp file

---
+++ 17_18/ASD_lab/mat.cpp
@@ -0,0 +1,8 @@
+// Task: Given a template of string (chosen from *[\*A-Z]), find minimum length
+// // of a text that fits in the template regardless of where it is matched
+
+// Approach:
+
+#include <cstdio>
+
+int main() { return 0; }
