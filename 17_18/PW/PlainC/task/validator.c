4643201c9bf607397b54f967a26b70c2efac2701
mbalc <mbalc909@gmail.com>
2018-01-16 01:10:11 +0100

    [PW] task 3 - read first lines in validator input

--- 17_18/PW/PlainC/task/validator.c
+++ 17_18/PW/PlainC/task/validator.c
@@ -1,7 +1,29 @@
-#include <cstdio>
+#include <stdio.h>
+#include <stdlib.h>
+// #include <map>
 
+int N, A, Q, U, F_count, start_state;
+
+const int MAX_STATE_COUNT = 100, MAX_ALPHA_SIZE = 'z' - 'a';
+// std::map<pair<int, int>, int> F;
+
+void read() {
+  char *line;
+  size_t buffer_size = 32;
+  line = (char*)calloc(buffer_size, sizeof(char));
+
+  scanf ("%d%d%d%d%d\n", &N, &A, &Q, &U, &F_count);
+  scanf ("%d\n", &start_state);
+
+  getline(&line, &buffer_size, stdin);
+
+  for (int i = 0; i < N - 1; ++i) {
+    printf(">%s<\n", line);
+  }
+}
 
 int main () {
-  int N, A, Q, U, F;
-  scanf ("%d%d%d%d%d", )
+  read();
+
+  return 0;
 }
