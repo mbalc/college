454c7788063095465a5c68b2adf1b3791c1f4328
mbalc <mbalc909@gmail.com>
2018-03-19 16:03:38 +0100

    [SO1] Improve code comments

--- current/SO/tasks/1/sequence.asm
+++ current/SO/tasks/1/sequence.asm
@@ -14,8 +14,16 @@ section .data
     EXPECTED_NO_OF_ARGS equ 2  ; one argument expected (path to the file)
 
     checklist times ALPHABET_LIMIT db 0
-    ; every alphabet element has a corresponding byte in this checklist
-    ; during number-by-number analysis 
+    ; every alphabet element (a number) has a corresponding value in this
+    ; checklist during number-by-number analysis: 
+    ;   0 - a number didn't show in any permutation
+    ;   1 - in first run - number occured in the first permutation
+    ;   1 - during other runs - number should show in this permutation, but
+    ;                           haven't done so yet, or a number that shouldn't
+    ;                           show up did so 
+    ;   2 - in first run - too many occurences in the first permutation
+    ;   2 - during other runs - number should show in this permutation, and did so
+    ;   3 and above - too many occurences in some permutation
 
 section .bss
     file_buffer resb BUFFER_SIZE  ; file fragment we analyse at one moment
@@ -34,8 +42,9 @@ _start:
     jl _fail  ; opening the file ended up with an error
 
     mov r14, rax  ; r14 - file descriptor
-    xor r10, r10
-    xor r15, r15  ; r15 - zero when there's a permutation parsing in progress
+    xor r10, r10  ; r10 - file_buffer number value container for comparing
+    xor r15, r15  ; r15 - permutation parsing indicator, 
+                  ; equals zero when there's one in progress
 
 _read_file:
     mov rdi, r14
@@ -48,8 +57,8 @@ _read_file:
     je _end_of_file  ; we have already read everything
     jl _fail  ; file reading has failed for some reason
 
-    mov r13, rax  ; size of buffer read
-    add r13, file_buffer  ; the last character read
+    mov r13, rax  ; value of $rax is size of buffer read
+    add r13, file_buffer  ; r13 - iterator to last element in file_buffer
     mov r12, file_buffer  ; r12 - iterator for file_buffer content
     jmp _analyze_buffer
 
@@ -59,7 +68,7 @@ _end_of_file:
     jmp _end
 
 _analyze_buffer: 
-    xor r15, r15  ; permutation parsing in progress
+    xor r15, r15  ; indicate that a permutation parsing is in progress
     mov r10b, byte [r12]
     mov r8, checklist
     add r8, r10
@@ -73,7 +82,7 @@ _analyze_buffer:
     jnz _proceed
 
     xor rcx, rcx
-    mov r8, checklist
+    mov r8, checklist  ; r8 - iterator for checklist contents
     call _handle_zero
 
 _proceed:
@@ -83,13 +92,13 @@ _proceed:
     jmp _read_file
 
 _handle_zero:
-    mov r15, 1  ; indicate that there is no permutation pending
-    mov r11b, byte [r8]
+    mov r15, 1  ; we indicate that there is no permutation pending
+    mov r11b, byte [r8]  ; r11 - value of checklist cell corresponding to 0
 
     cmp r11b, 1
-    je _first_permutation
+    je _first_permutation  ; end of the first permutation in the file
     dec r11b
-    jmp _other_permutations  ; otherwise r11b has to be equal to 2
+    jmp _other_permutations  ; side note - r11b has to be equal to 2
 
 _first_permutation:
     cmp byte [r8], 0
